5d2707f45f2c6a40f3bbe35b04a9b4963ce628d8
samk <samuelkariuki1@student.moringaschool.com>
2023-06-15 21:36:29 +0300

    completed function

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(blocks) {
+    if (blocks >= 42) {
+      return blocks - 42;
+    } else {
+      return 42 - blocks;
+    }
+  }
+  
+  function distanceFromHqInFeet(blocks) {
+    return distanceFromHqInBlocks(blocks) * 264;
+  }
+  
+  function distanceTravelledInFeet(startBlock, endBlock) {
+    if (startBlock < endBlock) {
+      return (endBlock - startBlock) * 264;
+    } else {
+      return (startBlock - endBlock) * 264;
+    }
+  }
+  
+  function calculatesFarePrice(startBlock, endBlock) {
+    const distance = distanceTravelledInFeet(startBlock, endBlock);
+    if (distance <= 400) {
+      return 0;
+    } else if (distance > 400 && distance <= 2000) {
+      return (distance - 400) * 0.02;
+    } else if (distance > 2000 && distance <= 2500) {
+      return 25;
+    } else {
+      return 'cannot travel that far';
+    }
+  }
